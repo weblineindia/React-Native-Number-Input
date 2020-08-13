5c4f33e965cd36d07e96a00017249bccdd0d14d4
Contributor @ WeblineIndia <38062303+partners-wli@users.noreply.github.com>
2020-08-13 15:45:49 +0530

    Comment Added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
 1.0.0 (August 13, 2020)
 Initial release
+1.0.1 (August 13, 2020)
+Adding Comments For Business Information.
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+Author Name  :  WeblineIndia  |  https://www.weblineindia.com/
+
+For more such software development components and code libraries, visit us at
+https://www.weblineindia.com/software-development-resources.html 
+
+Our Github URL : https://github.com/weblineindia
+**/
 import React, { PureComponent } from 'react';
 import { TextInput } from 'react-native';
 import { code } from 'currency-codes';
